af4e2fea398fd4b2f90cd5afa0fcecd5e0693e69
alexey.lysiuk <alexey.lysiuk@gmail.com>
2024-12-13 18:30:26 +0200

    no message

---
+++ a6r-console.cpp
@@ -0,0 +1,62 @@
+#include <cassert>
+#include <iostream>
+#include <string>
+
+#include <libserialport.h>
+
+
+int main()
+{
+	sp_port** ports;
+	sp_return result = sp_list_ports(&ports);
+	assert(result == SP_OK);
+
+	for (size_t i = 0; ports[i]; ++i)
+	{
+		sp_port* port = ports[i];
+		const sp_transport transport = sp_get_port_transport(port);
+
+		if (transport != SP_TRANSPORT_USB)
+			continue;
+
+		int vid, pid;
+		result = sp_get_port_usb_vid_pid(port, &vid, &pid);
+		assert(result == SP_OK);
+
+		if (vid != 0x0483 || pid != 0x5740)
+			continue;
+
+		result = sp_open(port, SP_MODE_READ_WRITE);
+		assert(result == SP_OK);
+
+		std::string command, buffer;
+		buffer.resize(1024);
+
+		while (true)
+		{
+			std::cin >> command;
+
+			if (command.empty())
+				break;
+
+			const unsigned int timeout = 1000;
+
+			result = sp_blocking_write(port, command.c_str(), command.size(), timeout);
+			assert(result == command.size());
+
+			result = sp_blocking_read(port, &buffer[0], buffer.size(), timeout);
+			buffer[result] = '\0';
+
+			std::cout << buffer;
+		}
+
+		result = sp_close(port);
+		assert(result == SP_OK);
+
+		break;
+	}
+
+	sp_free_port_list(ports);
+
+	return 0;
+}
