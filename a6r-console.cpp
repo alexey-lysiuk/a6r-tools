1af0026c710d5f304eaa5dcc131c2433e9418b05
alexey.lysiuk <alexey.lysiuk@gmail.com>
2024-12-14 10:52:55 +0200

    no message

--- a6r-console.cpp
+++ a6r-console.cpp
@@ -5,6 +5,70 @@
 #include <libserialport.h>
 
 
+static void RunInteractiveMode(sp_port* port)
+{
+	constexpr unsigned int TIMEOUT = 100;
+	std::string command = "help";
+
+	constexpr size_t BUFFER_SIZE = 1024;
+	char buffer[BUFFER_SIZE];
+
+	while (true)
+	{
+		command += '\r';
+
+		sp_return result = sp_blocking_write(port, command.c_str(), command.size(), TIMEOUT);
+		assert(result == command.size());
+
+		while (true)
+		{
+			result = sp_blocking_read(port, &buffer[0], BUFFER_SIZE, TIMEOUT);
+
+			if (result == SP_OK)
+				break;
+			else if (result < 0)
+			{
+				assert(false);
+				return;
+			}
+			else
+				buffer[result] = '\0';
+
+			std::cout << &buffer[command.size() + 1];
+		}
+
+		std::cin >> command;
+
+		if (command.empty())
+			break;
+	}
+}
+
+static bool Run(sp_port* port)
+{
+	const sp_transport transport = sp_get_port_transport(port);
+
+	if (transport != SP_TRANSPORT_USB)
+		return false;
+
+	int vid, pid;
+	sp_return result = sp_get_port_usb_vid_pid(port, &vid, &pid);
+	assert(result == SP_OK);
+
+	if (vid != 0x0483 || pid != 0x5740)
+		return false;
+
+	result = sp_open(port, SP_MODE_READ_WRITE);
+	assert(result == SP_OK);
+
+	RunInteractiveMode(port);
+
+	result = sp_close(port);
+	assert(result == SP_OK);
+
+	return true;
+}
+
 int main()
 {
 	sp_port** ports;
@@ -13,60 +77,8 @@ int main()
 
 	for (size_t i = 0; ports[i]; ++i)
 	{
-		sp_port* port = ports[i];
-		const sp_transport transport = sp_get_port_transport(port);
-
-		if (transport != SP_TRANSPORT_USB)
-			continue;
-
-		int vid, pid;
-		result = sp_get_port_usb_vid_pid(port, &vid, &pid);
-		assert(result == SP_OK);
-
-		if (vid != 0x0483 || pid != 0x5740)
-			continue;
-
-		result = sp_open(port, SP_MODE_READ_WRITE);
-		assert(result == SP_OK);
-
-		std::string command = "help";
-
-		static const size_t BUFFER_SIZE = 1024;
-		char buffer[BUFFER_SIZE];
-
-		static const unsigned int timeout = 100;
-
-		while (true)
-		{
-			command += '\r';
-
-			result = sp_blocking_write(port, command.c_str(), command.size(), timeout);
-			assert(result == command.size());
-
-			while (true)
-			{
-				result = sp_blocking_read(port, &buffer[0], BUFFER_SIZE, timeout);
-
-				if (result == 0)
-					break;
-				else if (result < 0)
-					exit(1);  // TODO
-				else
-					buffer[result] = '\0';
-
-				std::cout << &buffer[command.size() + 1];
-			}
-
-			std::cin >> command;
-
-			if (command.empty())
-				break;
-		}
-
-		result = sp_close(port);
-		assert(result == SP_OK);
-
-		break;
+		if (Run(ports[i]))
+			break;
 	}
 
 	sp_free_port_list(ports);
