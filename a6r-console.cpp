d0f54f3c6675d89619b690171aa5934e7060533d
alexey.lysiuk <alexey.lysiuk@gmail.com>
2024-12-20 15:30:23 +0200

    Revert "no message"
    This reverts commit e2b9766904a9f9e72c61b6374316ab57e20ca0a7.

--- a6r-console.cpp
+++ a6r-console.cpp
@@ -1,95 +1,92 @@
-//#include <exception>
-//#include <iostream>
-//#include <string>
+#include <exception>
+#include <iostream>
+#include <string>
 
-//#include "argparse.hpp"
-//#include "tinysa4.h"
-#include "bmpfile.h"
+#include "argparse.hpp"
+#include "tinysa4.h"
 
 
-//static void SendReceive(TinySA4& device, std::string& command)
-//{
-//	command += '\r';
-//
-//	const size_t commandsize = command.size();
-//
-//	constexpr size_t BUFFER_SIZE = 1024;
-//	char buffer[BUFFER_SIZE];
-//
-//	if (device.Send(command.c_str(), commandsize) != commandsize)
-//		std::cerr << "Incomplete send to device" << std::endl;  // TODO: make it fatal?
-//
-//	while (true)
-//	{
-//		const size_t read = device.Receive(&buffer[0], BUFFER_SIZE - 1);
-//
-//		if (read == 0)
-//			break;
-//		else
-//			buffer[read] = '\0';
-//
-//		std::cout << &buffer[commandsize + 1];  // skip repeated command
-//	}
-//}
-//
-//static void RunInteractiveMode(TinySA4& device)
-//{
-//	std::cout << "Type 'exit' to leave interactive mode" << std::endl;
-//
-//	std::string command = "help";
-//
-//	while (true)
-//	{
-//		SendReceive(device, command);
-//
-//		std::cin >> command;
-//
-//		if (command == "exit")
-//			break;
-//	}
-//}
+static void SendReceive(TinySA4& device, std::string& command)
+{
+	command += '\r';
+
+	const size_t commandsize = command.size();
+
+	constexpr size_t BUFFER_SIZE = 1024;
+	char buffer[BUFFER_SIZE];
+
+	if (device.Send(command.c_str(), commandsize) != commandsize)
+		std::cerr << "Incomplete send to device" << std::endl;  // TODO: make it fatal?
+
+	while (true)
+	{
+		const size_t read = device.Receive(&buffer[0], BUFFER_SIZE - 1);
+
+		if (read == 0)
+			break;
+		else
+			buffer[read] = '\0';
+
+		std::cout << &buffer[commandsize + 1];  // skip repeated command
+	}
+}
+
+static void RunInteractiveMode(TinySA4& device)
+{
+	std::cout << "Type 'exit' to leave interactive mode" << std::endl;
+
+	std::string command = "help";
+
+	while (true)
+	{
+		SendReceive(device, command);
+
+		std::cin >> command;
+
+		if (command == "exit")
+			break;
+	}
+}
 
 int main(int argc, char** argv)
 {
-//	argparse::ArgumentParser args("a6r-console", "0.0.1");
-//	auto& group = args.add_mutually_exclusive_group(true);
-//	group.add_argument("-c", "--command").metavar("COMMAND").append().help("execure command(s)");
-//	group.add_argument("-i", "--interactive").help("enter interactive mode").flag();
-//
-//	try
-//	{
-//		args.parse_args(argc, argv);
-//	}
-//	catch (const std::exception& ex)
-//	{
-//		std::cerr << ex.what() << std::endl << args;
-//		return EXIT_FAILURE;
-//	}
-//
-//	try
-//	{
-//		TinySA4 device;
-//
-//		if (args.get<bool>("--interactive"))
-//			RunInteractiveMode(device);
-//		else
-//		{
-//			auto commands = args.get<std::vector<std::string>>("--command");
-//
-//			for (std::string& command : commands)
-//			{
-//				SendReceive(device, command);
-//				std::cout << std::endl;
-//			}
-//		}
-//	}
-//	catch (const std::exception& ex)
-//	{
-//		std::cerr << ex.what() << std::endl;
-//		return EXIT_FAILURE;
-//	}
+	argparse::ArgumentParser args("a6r-console", "0.0.1");
+	auto& group = args.add_mutually_exclusive_group(true);
+	group.add_argument("-c", "--command").metavar("COMMAND").append().help("execure command(s)");
+	group.add_argument("-i", "--interactive").help("enter interactive mode").flag();
 
-	BMPFile f(argv[1]);
+	try
+	{
+		args.parse_args(argc, argv);
+	}
+	catch (const std::exception& ex)
+	{
+		std::cerr << ex.what() << std::endl << args;
+		return EXIT_FAILURE;
+	}
+
+	try
+	{
+		TinySA4 device;
+
+		if (args.get<bool>("--interactive"))
+			RunInteractiveMode(device);
+		else
+		{
+			auto commands = args.get<std::vector<std::string>>("--command");
+
+			for (std::string& command : commands)
+			{
+				SendReceive(device, command);
+				std::cout << std::endl;
+			}
+		}
+	}
+	catch (const std::exception& ex)
+	{
+		std::cerr << ex.what() << std::endl;
+		return EXIT_FAILURE;
+	}
 
 	return EXIT_SUCCESS;
 }
